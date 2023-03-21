948c42c1ea6eab4040eef8081e1ba2d21d0d6daf
Yuxuan Zhou <43881388+ZhouYuxuanYX@users.noreply.github.com>
2023-03-22 00:35:06 +0800

    Add files via upload

---
+++ ensemble.sh
@@ -0,0 +1,10 @@
+#python ensemble.py --dataset ntu120/xset --joint-dir work_dir/ntu120/cset/hyperformer \
+#--bone-dir work_dir/ntu120/cset/hyperformer_bone \
+#--joint-motion-dir work_dir/ntu120/cset/hyperformer_vel  \
+#--bone-motion-dir work_dir/ntu120/cset/hyperformer_bone_vel
+
+
+#python ensemble.py --dataset NW-UCLA --joint-dir work_dir/ucla/hyperformer \
+#--bone-dir work_dir/ucla/hyperformer_bone \
+#--joint-motion-dir work_dir/ucla/hyperformer__vel  \
+#--bone-motion-dir work_dir/ucla/hyperformer_bone_vel
---
+++ train.sh
@@ -0,0 +1,16 @@
+##!/bin/bash
+
+#python main.py  \
+#--config config/nturgbd120-cross-subject/joint.yaml --model model.Hyperformer.Model --work-dir work_dir/ntu120/csub/Hyperformer_joint --device 2 3 --base-lr 2.5e-2 --seed 2 
+
+#python main.py  \
+#--config config/nturgbd120-cross-set/joint.yaml --model model.Hyperformer.Model --work-dir work_dir/ntu120/cset/Hyperformer_joint --device 2 3 --base-lr 2.5e-2 --seed 2 
+
+#python main.py  \
+#--config config/nturgbd-cross-view/joint.yaml --model model.Hyperformer.Model --work-dir work_dir/ntu60/cview/Hyperformer_joint  --device 0 1 --base-lr 2.5e-2 --seed 2
+
+python main.py  \
+--config config/nturgbd-cross-subject/joint.yaml --model model.Hyperformer.Model --work-dir work_dir/ntu60/csub/Hyperformer_joint  --device 0 1 --base-lr 2.5e-2 --seed 2
+
+#python main.py  \
+#--config config/ucla/joint.yaml --model model.Hyperformer.Model --work-dir work_dir/ucla/Hyperformer_joint  --device 0 1 --base-lr 2.5e-2 --seed 2
