3eb7914f99cea675f0fc3ee553f936670f89ba08
Yuxuan Zhou <43881388+ZhouYuxuanYX@users.noreply.github.com>
2023-03-24 20:01:24 +0800

    Update ensemble.sh

--- ensemble.sh
+++ ensemble.sh
@@ -3,8 +3,3 @@
 #--joint-motion-dir work_dir/ntu120/cset/hyperformer_vel  \
 #--bone-motion-dir work_dir/ntu120/cset/hyperformer_bone_vel
 
-
-#python ensemble.py --dataset NW-UCLA --joint-dir work_dir/ucla/hyperformer \
-#--bone-dir work_dir/ucla/hyperformer_bone \
-#--joint-motion-dir work_dir/ucla/hyperformer__vel  \
-#--bone-motion-dir work_dir/ucla/hyperformer_bone_vel
