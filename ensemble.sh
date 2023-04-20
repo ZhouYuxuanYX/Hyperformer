37c21e097bf812146fb6218965444bf4f80edf66
Yuxuan Zhou <43881388+ZhouYuxuanYX@users.noreply.github.com>
2023-04-20 17:19:36 +0800

    Update ensemble.sh

--- ensemble.sh
+++ ensemble.sh
@@ -1,5 +1,20 @@
-#python ensemble.py --dataset ntu120/xset --joint-dir work_dir/ntu120/cset/hyperformer \
-#--bone-dir work_dir/ntu120/cset/hyperformer_bone \
-#--joint-motion-dir work_dir/ntu120/cset/hyperformer_vel  \
-#--bone-motion-dir work_dir/ntu120/cset/hyperformer_bone_vel
+python ensemble.py --dataset ntu120/xset --joint-dir work_dir/ntu120/cset/hyperformer \
+--bone-dir work_dir/ntu120/cset/hyperformer_bone \
+--joint-motion-dir work_dir/ntu120/cset/hyperformer_vel  \
+--bone-motion-dir work_dir/ntu120/cset/hyperformer_bone_vel
 
+python ensemble.py --dataset ntu120/xsub --joint-dir work_dir/ntu120/csub/hyperformer \
+--bone-dir work_dir/ntu120/csub/hyperformer_bone \
+--joint-motion-dir work_dir/ntu120/csub/hyperformer_vel  \
+--bone-motion-dir work_dir/ntu120/csub/hyperformer_bone_vel
+
+python ensemble.py --dataset ntu/xsub --joint-dir work_dir/ntu60/csub/hyperformer \
+--bone-dir work_dir/ntu60/csub/hyperformer_bone \
+--joint-motion-dir work_dir/ntu60/csub/hyperformer_vel  \
+--bone-motion-dir work_dir/ntu60/csub/hyperformer_bone_vel
+
+
+python ensemble.py --dataset ntu/xview --joint-dir work_dir/ntu60/cview/hyperformer \
+--bone-dir work_dir/ntu60/cview/hyperformer_bone \
+--joint-motion-dir work_dir/ntu60/cview/hyperformer_vel  \
+--bone-motion-dir work_dir/ntu60/cview/hyperformer_bone_vel
