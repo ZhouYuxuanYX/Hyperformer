beec18d2c90f8d4395d18d77426cfe3fe6839ddc
Yuxuan Zhou <43881388+ZhouYuxuanYX@users.noreply.github.com>
2023-04-24 22:34:42 +0800

    add bash script for evaluation

---
+++ evaluate.sh
@@ -0,0 +1,2 @@
+python main.py --weights hyperformer_pretrained_weights/ntu120/csub/joint/runs-134-131855.pt --phase test --save-score True \
+--config config/nturgbd120-cross-subject/joint.yaml --model model.Hyperformer.Model --work-dir work_dir/ntu120/csub/joint  --device 6 7
